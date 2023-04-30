ab8bd8b2f3a5892320ef23187dfd36a1a0cd557b
Kostya Dubyna <kosensopay@gmail.com>
2023-04-30 17:08:22 +0300

    Home work JS

---
+++ 7-switch-Math.js
@@ -0,0 +1,37 @@
+// 7.  За допомогою switch реалізуйте обчислення виразу, передаючи у switch, як параметр, символ математичної операції.
+//     Математичні операції для обчислення: "+", "-", "*", "/".
+//     Результат виводити в консоль.
+
+let number = 3;
+switch(number*3 && number+6 && 12-number && 27 / 3){
+    case 1:
+        console.log(1)
+        break;
+    case 2:
+        console.log(2)
+        break;
+    case 3:
+        console.log(3)
+        break;
+    case 4:
+        console.log(4)
+        break;
+    case 5:
+        console.log(5)
+        break;
+    case 6:
+        console.log(6)
+        break;
+    case 7:
+        console.log(7)
+        break;
+    case 8:
+        console.log(8)
+        break;
+    case 9:
+        console.log(9)
+        break;   
+    default:
+        console.log('Science!') 
+        break;         
+}
---
+++ 8-switchRegExp.js
@@ -0,0 +1,6 @@
+// 8.*  Використовуючи властивості рядків (тип string), та регулярний вираз (regular expression) видалити голосні букви зі слова.
+someText = 'My homework'
+someNewText = 'This is the second example'
+console.log(someText.replace(/[aeiouy]/gi, ''))
+console.log(someNewText.replace(/[thscxmplnd]/gi, ''))
+
